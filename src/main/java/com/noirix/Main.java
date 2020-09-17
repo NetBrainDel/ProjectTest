6d5da66c7830b380a3784b9ff7235df6a9df0cae
Alexander <tsybinsasha5@gmail.com>
2020-09-17 14:56:46 +0300

    commit dev

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("Dev changes cherri pick!");
         System.out.println("Hello, world!1111111");
         System.out.println("String for cherry pick");
-
+        System.out.println("SUPER");
     }
 }
