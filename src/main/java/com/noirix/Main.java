3164b168a40a50e69d9955d0b4099a6e0042c921
Alexander <tsybinsasha5@gmail.com>
2020-09-17 15:59:09 +0300

    Merge branch 'master' of https://github.com/NetBrainDel/ProjectTest into dev
     Conflicts:
    	src/main/java/com/noirix/Main.java

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -15,8 +15,6 @@ public class Main {
         System.out.println("Hello, world!1111111");
         System.out.println("String for cherry pick");
         System.out.println("SUPER");
-        System.out.println("TRYdev2");
-        System.out.println("dev1");
         System.out.println("dev2");
 
     }
