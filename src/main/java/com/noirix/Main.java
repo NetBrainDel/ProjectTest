c1b8c1f667621f80a3d2a7e3325870e9add88823
Alexander <tsybinsasha5@gmail.com>
2020-09-24 20:09:56 +0300

    cherripick commit

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -10,17 +10,6 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!1111");
         System.out.println("Dev changes cherri pick!");
-        System.out.println("TEST");
-        System.out.println("Dev changes cherri pick!");
-        System.out.println("Hello, world!1111111");
-        System.out.println("String for cherry pick");
-        System.out.println("SUPER");
-        System.out.println("dev1");
-        System.out.println("and");
-        System.out.println("master1");
-        System.out.println("dev2");
-        System.out.println("TRYdev2");
-        System.out.println("master");
-        System.out.println("cherri pick");
+
     }
 }
